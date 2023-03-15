a3b3d9d24555818df4776a330ec618393a3f0fef
BharathRoyals <sbharath2703@gmail.com>
2023-03-15 23:12:49 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def dhanu(request):
+    return render(request,'dhanu.html')
+
+def hemanth(request):
+    return render(request,'hemanth.html')   
